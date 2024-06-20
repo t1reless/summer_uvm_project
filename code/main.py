80e1d2b4550f41608c6347dc3a0a2cf23b155ef4
t1reless <d3v@tuta.com>
2024-06-20 11:50:37 -0400

    managed to hacked together a working prototype which records tweets which contain words from the sample list- now for the fun part. extracting words which occur at indices before and after (kill me)

--- code/main.py
+++ code/main.py
@@ -16,25 +16,36 @@ else:
         print(len(tweets))
 """
 
-seed_words = []
+seed_words_list = []
 tweets = []
+word_matches_and_frequency = {}
+
 # create a function to load in a list of seed words into memory
 def seed_lexicon_reader(txt_file_name):
     with open(txt_file_name) as file:
         lines = file.readlines()
         for word in lines:
-            seed_words.append(word.strip())
+            seed_words_list.append(word.strip())
+
+
+def tweet_csv_reader(file_name):
+    all_tweets = pd.read_csv(file_name).columns
+    for tweet in all_tweets:
+        tweets.append(tweet.split())
+
+def finding_seed_words_in_tweets():
+    words = set(seed_words_list)
+    for tweet_list in tweets:
+        result = [x for x in tweet_list if x in words]
+        if len(result) == 0:
+            continue
+        else:
+            print(f"{" ".join(str(x)for x in tweet_list)} contained: {result}")
+
 seed_lexicon_reader("seed_words.txt")
-print(seed_words)
-
-def seed_lexicon_csv_reader(file_name):
-    seed_lexicon = pd.read_csv(file_name).columns
-    for word in seed_lexicon:
-        seed_words.append(word)
-
-
+tweet_csv_reader("negative.csv")
+finding_seed_words_in_tweets()
 # create a function to load a given csv into memory 
-# 
 # then iterate over the values 
 # 1. chunk the tweet into a list splitting on spaces
 # 2. compare the words in the list with the list of seed words 
