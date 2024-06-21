748f0254559d729e4065ea08c28c077621aed13a
t1reless <d3v@tuta.com>
2024-06-21 13:54:52 -0400

    Working version

--- code/main-v0.py
+++ code/main-v0.py
@@ -60,18 +60,3 @@ tweet_csv_reader("negative.csv")
 finding_seed_words_in_tweets()
 
 print(dictionary_sorter(word_matches_and_frequency))
-# create a function to load a given csv into memory 
-# then iterate over the values 
-# 1. chunk the tweet into a list splitting on spaces
-# 2. compare the words in the list with the list of seed words 
-# 
-# ctrl flow:
-# if there is a match then check the next two indices against the output
-# dictionary. 
-#   if already in the dictionary increase the count val by 1, 
-# else:
-#   add it and set the val to one. 
-
-
-# create a function to print the complete dictionary with all words that occur over 
-# n times
--- code/main-v1.py
+++ code/main-v1.py
@@ -5,11 +5,18 @@ from numpy import loadtxt
 from collections import Counter
 import sys
 
+#arguments
 seed_file = sys.argv[1]
 tweet_candidate_file = str(sys.argv[2])
+lower_bound = int(sys.argv[3])
 
+#storage 
 verified_tweets = []
+temp_list = []
 grand_list = []
+
+#functions
+
 # reads the list of seed words and writes it to a tuple 
 def seed_list_reader(name):
     with open(name,"r") as file:
@@ -40,7 +47,11 @@ def corpus_maximus():
         for word in tweet:
             grand_list.append(word)
 
-
+#returns a new list excluding words that occur under n number of times
+def lower_bound_filter(lower_bound):
+    working_dict = Counter(grand_list)
+    new_dict = {key:val for key, val in working_dict.items() if val >= lower_bound}
+    print(Counter(new_dict))
 
 seed_words = seed_list_reader(seed_file)
 tweets = tweet_csv_parser(tweet_candidate_file)
@@ -48,6 +59,7 @@ tweets = tweet_csv_parser(tweet_candidate_file)
 compare_tweet_to_seeds(seed_words,tweets)
 
 corpus_maximus()
+
 # prints a dictionary of all words in all tweets which contain the seed words 
 # and their counts
-print(Counter(grand_list))
+lower_bound_filter(lower_bound)
