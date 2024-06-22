b77e7f9d21bea8c52c4ab63614426a7148939fd8
t1reless <d3v@tuta.com>
2024-06-21 21:17:09 -0400

    Revert to c4b2c7a

---
+++ code/main-v0.py
@@ -0,0 +1,62 @@
+import csv
+import pandas as pd 
+
+
+
+tweets = []
+word_matches_and_frequency = {}
+
+def words_surrounding_key_word_in_tweet(word,tweet_as_list):
+    position = tweet_as_list.index(word)
+    next_words = []
+    try:
+        next_words.append(tweet_as_list[position+1])
+        next_words.append(tweet_as_list[position+2])
+    except:
+        IndexError
+    else: 
+        return next_words
+
+# loads all tweets into a list as a list of lists where the larger list is the tweets and the smaller lists are each individual word
+def seed_lexicon_reader(txt_file_name):
+    seed_words_list = []
+    with open(txt_file_name) as file:
+        lines = file.readlines()
+        for word in lines:
+            seed_words_list.append(word.strip())
+        return tuple(seed_words_list)
+seed_words_tup = seed_lexicon_reader("seed_words.txt")
+
+def tweet_csv_reader(file_name):
+    all_tweets = pd.read_csv(file_name).columns
+    for tweet in all_tweets:
+        tweets.append(tweet.split())
+
+# function to locate th
+def finding_seed_words_in_tweets():
+    words = set(seed_words_tup)
+    for tweet_list in tweets:
+        result = [x for x in tweet_list if x in words]
+        if len(result) == 0:
+            continue
+        else:
+            close_words = words_surrounding_key_word_in_tweet(result[0],tweet_list)
+            if close_words == None:
+                continue
+            else:
+                for i in range(0,2):
+                    word = close_words[i]
+                    keys = word_matches_and_frequency.keys()
+                    if word in keys:
+                        word_matches_and_frequency.update({word:word_matches_and_frequency[word]+1})
+                    else:
+                        word_matches_and_frequency[word] = 1
+
+def dictionary_sorter(input):
+    sorted_dict = dict(sorted(input.items(), key = lambda item: item[1]))
+    return sorted_dict
+
+tweet_csv_reader("negative.csv")
+finding_seed_words_in_tweets()
+
+print(dictionary_sorter(word_matches_and_frequency))
---
+++ code/main-v1.py
@@ -0,0 +1,78 @@
+#imports
+import pandas as pd 
+import csv
+from numpy import loadtxt
+from collections import Counter
+import sys
+import matplotlib.pyplot as plt
+
+#arguments
+seed_file = sys.argv[1]
+tweet_candidate_file = str(sys.argv[2])
+lower_bound = int(sys.argv[3])
+
+#storage 
+verified_tweets = []
+temp_list = []
+grand_list = []
+
+#functions
+
+# reads the list of seed words and writes it to a tuple 
+def seed_list_reader(name):
+    with open(name,"r") as file:
+        lines = tuple(file.read().split())
+        return lines
+
+# reads csv of input tweets and returns a list of tuples
+def tweet_csv_parser(name):
+    tweets = []
+    all_tweets = pd.read_csv(name).columns
+    for tweet in all_tweets:
+        tweets.append(tuple(tweet.split()))
+    return tweets
+
+# accepts a tuple of seed words and a list of tuples and checks for intersection
+# if there is intersection then it adds them to a list of verified tweets
+def compare_tweet_to_seeds(seeds,candidates):
+    for tweet in candidates: 
+        test = list(set(tweet)&set(seeds))
+        if len(test) >= 1:
+            verified_tweets.append(tweet)
+        else: 
+            continue
+
+# converts the list of verified tweets into a list containing all words
+def corpus_maximus():
+    for tweet in verified_tweets:
+        for word in tweet:
+            grand_list.append(word)
+
+#returns a new list excluding words that occur under n number of times
+def lower_bound_filter(lower_bound):
+    working_dict = Counter(grand_list)
+    new_dict = {key:val for key, val in working_dict.items() if val >= lower_bound}
+    return Counter(new_dict)
+
+seed_words = seed_list_reader(seed_file)
+tweets = tweet_csv_parser(tweet_candidate_file)
+
+compare_tweet_to_seeds(seed_words,tweets)
+
+corpus_maximus()
+
+# prints a dictionary of all words in all tweets which contain the seed words 
+# and their counts
+data = lower_bound_filter(lower_bound)
+
+words = list(data.keys())
+frequ = list(data.values())
+
+plt.bar(words,frequ)
+
+plt.xlabel("Words")
+plt.ylabel("Frequency")
+plt.title(f"Words occuring more than {lower_bound} times in {tweet_candidate_file}")
+
+print(lower_bound_filter(lower_bound))
+plt.show()
